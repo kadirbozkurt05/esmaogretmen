0ee7e09be1b7b4d48cce57f22040d7e0e4d33646
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-05-25 12:34:15 +0200

    NETLIFY SERVERLESS

---
+++ server/netlify/functions/api.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+import userRouter from "../../src/routes/userRoutes.js";
+import competitionRouter from "../../src/routes/competitionRoutes.js";
+import newsRouter from "../../src/routes/newsRoutes.js";
+
+
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+api.use(express.urlencoded({ limit: '50mb' }));
+
+api.use("/api", userRouter);
+api.use("/api", competitionRouter);
+api.use("/api", newsRouter);
+
+
+export const handler = serverless(api);
