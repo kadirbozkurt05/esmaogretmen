33146cf92d3584987b31946f2fd7ecaa530b232f
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-06-02 13:30:32 +0200

    dist

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ limit: '50mb' }));
 
+app.use(express.static('dist'))
+
+
 app.use("/api", userRouter);
 app.use("/api", competitionRouter);
 app.use("/api", newsRouter);
