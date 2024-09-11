28085f1dbcae39c77d5dbbbd6349c02b7dbdd7de
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-09-12 01:02:41 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,8 @@ export default defineConfig({
       '/api':'http://localhost:5000/'
     }
   },
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-      ]
-    }
+  build:{
+    outDir: "build"
   }
 
 })
