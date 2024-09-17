1a21ec3ae24cab6ec2bd399e68d014fd04d1c798
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2024-09-18 00:12:45 +0200

    first commit

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -22,7 +22,7 @@ export default function UserProvider({ children }) {
         headers: {
           "Content-Type": "application/json",
         },
-        credentials: "include",
+        credentials:"include",
       });
 
       if (response.ok) {
